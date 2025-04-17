c811debacf48d562840fc33e9ff63159c3c4ad54
Xiaoming Xi <xiaomingxi@sifli.com>
2025-04-17 16:58:51 +0800

    delete bluetooth

--- middleware/bluetooth/include/bf0_sibles_console.h
+++
@@ -1,7 +0,0 @@
-#ifndef _BF0_SIBLES_CONSOLE_H
-#define _BF0_SIBLES_CONSOLE_H
-
-int rt_ble_serial_dev_register(const char *name);
-
-#endif // _BF0_SIBLES_CONSOLE_H
-
--- middleware/bluetooth/lib/dummy.c
+++
@@ -1,3 +0,0 @@
-#include "bf0_lib.h"
-
-
