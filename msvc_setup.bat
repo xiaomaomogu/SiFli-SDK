103b7308054a077ad82bc5d10958806f4d8cdc72
yxhuang <yxhuang@sifli.com>
2025-04-27 21:26:09 +0800

    [int][bug][pc] Fix simulator building error with VS community version
    Project: SiFliSDK
    
    redmine: #id, REDMINE-id
    
    ext-redmine: REDMINE-4543
    
    [Description in detail]
    
    Affected branch: [main]
    
    Change-Id: Ia5a27178374eea3fa3f338c78a475d8e51f303a0

--- msvc_setup.bat
+++ msvc_setup.bat
@@ -1,6 +1,6 @@
 @REM Setup MSVC PATH
 @REM MSVC root folder
-subst x: "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Tools\MSVC\14.16.27023"
+subst x: "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023"
 @REM Windows SDK Include folder
 subst y: "C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0"
 @REM Windows SDK Lib folder
