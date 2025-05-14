9e0069baafcf1bcdcc6d41fbf958cdd2d3ae88a2
HalfSweet <haoranjiang@sifli.com>
2025-05-14 10:05:40 +0800

    [rel][new][bsp] Yellow Mountai Add Charge Chip Initialization
    Project: SiFliSDK
    
    redmine: #id, REDMINE-id
    
    ext-redmine: #4730, REDMINE-4730
    
    [Description in detail]
    
    Affected branch: [main]
    
    Change-Id: Ieb9c6d95302687da8ca5c20151741d75521787b1

--- customer/boards/yellow_mountain_base/bsp_power.c
+++ customer/boards/yellow_mountain_base/bsp_power.c
@@ -1,5 +1,5 @@
 #include "bsp_board.h"
-
+#include <stdint.h>
 
 void BSP_GPIO_Set(int pin, int val, int is_porta)
 {
@@ -25,7 +25,62 @@ __WEAK void BSP_PowerDownCustom(int coreid, bool is_deep_sleep)
 
 __WEAK void BSP_PowerUpCustom(bool is_deep_sleep)
 {
+    // VSYS
+    BSP_GPIO_Set(38, 1, 1); 
 
+    // VSYS to VCC_3V3
+    BSP_GPIO_Set(26, 1, 1);
+
+    // GS_3V3
+    BSP_GPIO_Set(30, 1, 1);
+    HAL_Delay_us(500); // must delay 500us to make sure power on finish
+
+    // Configure Charger
+    const uint8_t charger_addr = 0x49;
+    HAL_StatusTypeDef ret;
+    I2C_HandleTypeDef i2c_Handle;
+    HAL_RCC_EnableModule(RCC_MOD_I2C2);
+    i2c_Handle.Instance = I2C2;
+    i2c_Handle.Mode = HAL_I2C_MODE_MASTER; // i2c master mode
+    i2c_Handle.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT; // i2c 7bits device address mode
+    i2c_Handle.Init.ClockSpeed = 400000; // i2c speed (hz)
+    i2c_Handle.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+    i2c_Handle.Init.OwnAddress1 = 0;
+    ret = HAL_I2C_Init(&i2c_Handle);
+    if (ret != HAL_OK)
+    {
+        goto exit;
+    }
+
+    __HAL_I2C_ENABLE(&i2c_Handle);
+    uint8_t data = 0;
+
+    // CEB, address 0x01, bit3
+    ret = HAL_I2C_Mem_Read(&i2c_Handle, charger_addr, 0x01, I2C_MEMADD_SIZE_8BIT, &data, 1, 1000);
+    if (ret != HAL_OK)
+    {
+        goto exit;
+    }
+    data &= ~(1 << 3); // clear bit3
+    ret = HAL_I2C_Mem_Write(&i2c_Handle, charger_addr, 0x01, I2C_MEMADD_SIZE_8BIT, &data, 1, 1000);
+    if (ret != HAL_OK)
+    {
+        goto exit;
+    }
+    // disable watchdog, address 0x05, bit5-6
+    ret = HAL_I2C_Mem_Read(&i2c_Handle, charger_addr, 0x05, I2C_MEMADD_SIZE_8BIT, &data, 1, 1000);
+    if (ret != HAL_OK)
+    {
+        return;
+    }
+    data &= ~(3 << 5);
+    ret = HAL_I2C_Mem_Write(&i2c_Handle, charger_addr, 0x05, I2C_MEMADD_SIZE_8BIT, &data, 1, 1000);
+    if (ret != HAL_OK)
+    {
+        goto exit;
+    }
+exit:
+    __HAL_I2C_DISABLE(&i2c_Handle);
 }
 
 
@@ -40,8 +95,6 @@ void BSP_Power_Up(bool is_deep_sleep)
 #endif /* BSP_USING_PSRAM1 */
     }
 #endif  /* SOC_BF0_HCPU */
-
-
 }
 
 
@@ -57,11 +110,6 @@ void BSP_IO_Power_Down(int coreid, bool is_deep_sleep)
 #endif /* BSP_USING_PSRAM1 */
     }
 #endif  /* SOC_BF0_HCPU */
-
-
-
-
-
 }
 
 void BSP_SDIO_Power_Up(void)
