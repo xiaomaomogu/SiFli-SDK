3fe63f6aaa8340876207ac8874896df135a6b659
Xiaoming Xi <xiaomingxi@sifli.com>
2025-05-16 18:00:23 +0800

    [rel][opt][board] Delete em-lb566 and rename 56-devkit-lcd board
    Project: SiFliSDK
    
    ext-redmine: #4795, REDMINE-4795
    
    [Description in detail]
    
    Affected branch: [main]
    
    Change-Id: Ic846d55597357a9efb34587a5238b387fe5de5e7

--- customer/boards/em-lb566/hcpu/rtconfig.py
+++
@@ -1,9 +0,0 @@
-import os
-import argparse
-
-SIFLI_SDK=os.getenv('SIFLI_SDK')
-TARGET_NAME = 'bf0_ap'
-WERROR = True
-JLINK_DEVICE = 'SF32LB56X'
-CORE = "HCPU"
-CHIP = 'SF32LB56X'
--- customer/boards/em-lb566/lcpu/rtconfig.py
+++
@@ -1,8 +0,0 @@
-import os
-import argparse
-
-SIFLI_SDK=os.getenv('SIFLI_SDK')
-TARGET_NAME = 'lcpu'
-CORE = "LCPU"
-CHIP = 'SF32LB56X'
-
